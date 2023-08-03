7453c958e03e5e8ecef982642cbac0eb35f2f96b
Mateus Rabelo <mateusaqles@gmail.com>
2023-08-02 21:18:09 -0300

    ADD: added more config assets. ADD: new xml sign up page

--- app/src/main/java/com/teste/chatapp/MainActivity.kt
+++ app/src/main/java/com/teste/chatapp/MainActivity.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        setContentView(R.layout.activity_main)
     }
 }
