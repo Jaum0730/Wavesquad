8acab8e4b4cb1c8703bc8a9f139be2ebae414e28
Mateus Rabelo <mateusaqles@gmail.com>
2023-08-02 23:32:59 -0300

    ADD: button functions of main page now response

--- app/src/main/java/com/teste/chatapp/MainActivity.kt
+++ app/src/main/java/com/teste/chatapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.teste.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLogin: Button = findViewById(R.id.btnLogin)
+        val btnSignUp: Button = findViewById(R.id.btnSingUp)
+
+        btnLogin.setOnClickListener()
+        {
+            setContentView(R.layout.activity_log_in)
+        }
+
+        btnSignUp.setOnClickListener()
+        {
+            setContentView(R.layout.activity_sign_up)
+        }
     }
 }
+
