507034a385300edb6c2b2d1de03dc07ccc9ad7db
Mateus Rabelo <mateusaqles@gmail.com>
2023-08-08 12:44:57 -0300

    UPDATE: a minimal reforming in activit_log_in

--- app/src/main/java/com/teste/chatapp/LogInActivity.kt
+++ app/src/main/java/com/teste/chatapp/LogInActivity.kt
@@ -2,10 +2,14 @@ package com.teste.chatapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+
+        val txtLoginEmail: EditText = findViewById<EditText>(R.id.txtLoginEmail)
+        val txtLoginPassword: EditText = findViewById<EditText>(R.id.txtLoginPassword)
     }
 }
