4f22b00a115b01979e6d938ce4fe1762f4eb2fee
Mateus Rabelo <mateusaqles@gmail.com>
2023-08-15 17:19:45 -0300

    ADD: start to convert the layout to jetpack compose

--- app/src/main/java/com/teste/chatapp/MainActivity.kt
+++ app/src/main/java/com/teste/chatapp/MainActivity.kt
@@ -10,26 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnLogin: Button = findViewById(R.id.btnLogin)
-        val btnSignUp: Button = findViewById(R.id.btnSingUp)
-
-        btnLogin.setOnClickListener()
-        {
-            // Intent estava implemendata da maneira errada
-            // Tinha que ter importado o pluguin no gradle build.gradle (ChatApp)-> id 'com.android.library' version '8.1.0' apply false
-            val Intent = Intent(this, LogInActivity::class.java)
-            startActivity(Intent)
-        }
-
-        btnSignUp.setOnClickListener()
-        {
-
-            val Intent = Intent(this, SignUpActivity::class.java)
-            startActivity(Intent)
-        }
     }
-
-
-
 }
 
---
+++ app/src/main/java/com/teste/chatapp/compose/MenuLayout.kt
@@ -0,0 +1,89 @@
+package com.teste.chatapp
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.teste.chatapp.R
+
+@Composable
+fun LogInScreen(onContinueClick: () -> Unit) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.primary)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.img_background),
+            contentDescription = "troll",
+            contentScale = ContentScale.FillBounds,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.wave),
+                contentDescription = null,
+                modifier = Modifier
+                    .width(105.dp)
+                    .height(400.dp)
+                    .align(Alignment.Start)
+                    .padding(start = 35.dp)
+
+
+            )
+
+            Text(
+                text = "Bem vindo a WaveSquad :)",
+                color = MaterialTheme.colorScheme.onPrimary,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(top = 14.dp)
+
+            )
+
+
+            Button(
+                onClick = onContinueClick,
+                modifier = Modifier
+                    .padding(top = 20.dp)
+                    .fillMaxWidth()
+                    .height(48.dp)
+                    .clip(MaterialTheme.shapes.medium)
+                    .background(MaterialTheme.colorScheme.secondary),
+            ) {
+                Text(
+                    text = "Continuar",
+                    color = MaterialTheme.colorScheme.onSecondary,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun PreviewLogInScreen() {
+    LogInScreen(onContinueClick = {})
+}
