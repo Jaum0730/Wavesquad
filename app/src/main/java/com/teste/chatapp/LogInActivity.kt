7c0477a37f7265c4917c2b208a0a09c48aaa81a8
João Victor Lopes <64561972+Jaum0730@users.noreply.github.com>
2023-08-18 15:04:55 -0300

    FIX LogInActivity.kt
    FIX: git added a function twice unnecessarily

--- app/src/main/java/com/teste/chatapp/LogInActivity.kt
+++ app/src/main/java/com/teste/chatapp/LogInActivity.kt
@@ -16,17 +16,11 @@ private lateinit var dbAuth:FirebaseAuth
 class LogInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-<<<<<<< HEAD
-        setContent {
-            LoginLayoutScreenPreview()
-        }
-
-=======
         setContent{
             LoginLayoutScreenPreview()
         }
         //olhar utilidade
->>>>>>> 7a3e9b8cfecdf94b0baf4f8b1354e9bbc30dfae1
+
         dbAuth = FirebaseAuth.getInstance()
 
         var txtLoginEmail: EditText = findViewById<EditText>(R.id.txtLoginEmail)
